b15d63dcea9a4b09fc43322431fa10aa287ed3d9
Sebastian Wirkijowski <sebastian@wirkijowski.dev>
2026-06-09 14:20:21 +0200

    chore: add last flush time helper function

--- includes/helpers.php
+++ includes/helpers.php
@@ -4,6 +4,12 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
+function agent_monitor_last_flush_time() {
+    return file_exists( AGENT_MONITOR_EVENT_LOG_FLUSH_PATH )
+        ? filemtime( AGENT_MONITOR_EVENT_LOG_FLUSH_PATH )
+        : false;
+}
+
 function agent_monitor_is_enabled(): bool {
     return (bool) get_option( AGENT_MONITOR_IS_ENABLED, true )
         && (bool) get_option( AGENT_MONITOR_TOKEN );
